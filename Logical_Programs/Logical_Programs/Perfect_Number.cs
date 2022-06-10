83a7803b6facdd3b62d30676cf131d864868e08b
karishmask46 <karishmask.15999@gmail.com>
2022-06-10 15:48:44 +0530

    [Karishma]PerfectNum

---
+++ Logical_Programs/Logical_Programs/Perfect_Number.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    class Perfect_Number
+    {
+        public bool Factors(int Number, int potential_factor)
+        {
+            if (Number % potential_factor == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Perfectnum()
+        {
+            int check = 0;
+            Console.WriteLine("Enter the number you want to check: ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= Number / 2; i++)
+            {
+                if (Factors(Number, i))
+                {
+                    check += i;
+
+                }
+            }
+            if (check == Number)
+            {
+                Console.WriteLine(Number + " Is a Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine(Number + "is not a perfect Number");
+
+            }
+        }
+
+    }
+}
+
---
+++ Logical_Programs/Logical_Programs/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Logical_Programs
+{
+    class logicalProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for Perfect Number:");
+            Console.WriteLine("1 for a perfect number");
+            int hammer = Convert.ToInt32(Console.ReadLine());
+            switch (hammer)
+            {
+                case 1:
+                    Perfect_Number logic = new Perfect_Number();
+                    logic.Perfectnum();
+                    break;
+
+            }
+           
+           
+        }
+    }
+
+}
+
