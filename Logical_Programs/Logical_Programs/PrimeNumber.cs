9f6e9d9478ddac98c88e749e9e357f690e9c5f7c
karishmask46 <karishmask.15999@gmail.com>
2022-06-10 16:00:53 +0530

    [Karishma]Check num is prime or not

---
+++ Logical_Programs/Logical_Programs/PrimeNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    internal class PrimeNumber
+    {
+        public void Prime_Number()
+        {
+            Console.WriteLine("Enter The Number");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            int j = 0;
+
+            for (int i = 2; i < Num; i++)
+            {
+                if (Num % i == 0)
+                {
+                    j = 1;
+                    break;
+                }
+
+            }
+            if (j == 1)
+            {
+                Console.WriteLine(Num + " Is Not a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine(Num + "  Is a Prime Number");
+
+            }
+
+        }
+    }
+}
+
--- Logical_Programs/Logical_Programs/Program.cs
+++ Logical_Programs/Logical_Programs/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Enter a number for Perfect Number:");
             Console.WriteLine("1 for a perfect number");
             Console.WriteLine("2 for a reverse number");
+            Console.WriteLine("3 for a Prime Number7");
             int hammer = Convert.ToInt32(Console.ReadLine());
             switch (hammer)
             {
@@ -17,6 +18,10 @@
                     case 2:
                     Reverse_Number reverse = new Reverse_Number();
                     reverse.FindReverseNum();
+                    break;
+                case 3:
+                    PrimeNumber primeNum = new PrimeNumber();
+                    primeNum.Prime_Number();
                     break;
 
             }
